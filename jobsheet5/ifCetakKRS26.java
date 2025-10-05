af2c72992d66811e4ba3dcb53ddbd2d2ffe1adca
RifkyFebrian3140 <nightmon3140@gmail.com>
2025-10-05 13:08:25 +0700

    mantap

--- jobsheet5/ifCetakKRS26.java
+++ jobsheet5/ifCetakKRS26.java
@@ -1,3 +1,17 @@
 package jobsheet5;
 
 import java.util.Scanner;
+public class ifCetakKRS26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS Siakad ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
