808b43e3618902cf071ff272503aa1b60503f15a
RifkyFebrian3140 <nightmon3140@gmail.com>
2025-11-26 23:00:31 +0700

    New

---
+++ pertemuan14/Kubus25.java
@@ -0,0 +1,30 @@
+package pertemuan14;
+
+import java.util.Scanner;
+public class Kubus25 {
+
+public static int hitungVolume (int sisi) {
+    int volume = sisi*sisi*sisi;
+    return volume;
+    }
+public static int hitungLuarPermukaan (int sisi) {
+    int luasPermukaan = 6 * sisi * sisi;
+    return luasPermukaan;
+    }
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int sisi, vol, luasPermukaan;
+
+    System.out.println("Masukkan sisi");
+    sisi = input.nextInt();
+
+    vol = hitungVolume(sisi);
+    System.out.println("Volume kubus adalah" + vol);
+
+    luasPermukaan = hitungLuarPermukaan(sisi);
+    System.out.println("Luas permukaan kubus adalah" + luasPermukaan);
+
+    input.close();
+    }
+}
