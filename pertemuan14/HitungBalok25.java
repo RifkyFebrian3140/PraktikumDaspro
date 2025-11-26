b8a9f13bcb12659d759670fb97789fe8d28111d3
RifkyFebrian3140 <nightmon3140@gmail.com>
2025-11-26 22:44:38 +0700

    New

--- pertemuan14/HitungBalok25.java
+++ pertemuan14/HitungBalok25.java
@@ -1,11 +1,37 @@
 package pertemuan14;
 
-static int hitungLuas (int pjg, int lb) {
+import java.util.Scanner;
+public class HitungBalok25 {
+public static int hitungLuas (int pjg, int lb) {
     int luas = pjg * lb;
-    return Luas;
+    return luas;
 }
 
-static int hitungVolume (int tinggi, int a, int b) {
+public static int hitungVolume (int tinggi, int a, int b) {
     int volume = hitungLuas(a, b) * tinggi;
     return volume;
+}
+
+public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+
+    int p, l, t, L, vol;
+
+    System.out.println("Masukkan panjang");
+    p = input.nextInt();
+
+    System.out.println("Masukkan lebar");
+    l = input.nextInt();
+
+    System.out.println("Masukkan tinggi");
+    t = input.nextInt();
+
+    L = hitungLuas(p, l);
+    System.out.println("Luas persegi panjang adalah "+ L);
+
+    vol = hitungVolume(p, l, t);
+    System.out.println("Volume balok adalah "+ vol);
+
+    input.close();
+    }
 }
