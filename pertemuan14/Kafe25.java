c7cd5df16243da8723fd5b32e14adec37e4ec0f3
RifkyFebrian3140 <nightmon3140@gmail.com>
2025-11-26 13:51:13 +0700

    New

--- pertemuan14/Kafe25.java
+++ pertemuan14/Kafe25.java
@@ -29,4 +29,11 @@ public class Kafe25 {
     public static void main(String[] args) {
         Menu("Budi", true, "DISKON30");
     }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) { 
+        int [] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems [pilihanMenu -1] * banyakItem;
+        return hargaTotal;
+    }
 }
